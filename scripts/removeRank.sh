17513ac1bfd3d4ccde8213944dc4ef02c99de778
logicalmechanism <m.quinn.p@protonmail.com>
2022-09-03 16:19:37 -0700

    rank testing is complete

---
+++ scripts/removeRank.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -e
+
+# Paths
+export CARDANO_NODE_SOCKET_PATH=$(cat path_to_socket.sh)
+cli=$(cat path_to_cli.sh)
+script_path="../cogno-contract/cogno-contract.plutus"
+testnet_magic=$(cat data/testnet.magic)
+
+# Addresses
+script_address=$(${cli} address build --payment-script-file ${script_path} --testnet-magic ${testnet_magic})
+issuer_address=$(cat wallets/seller-wallet/payment.addr)
+issuer_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/seller-wallet/payment.vkey)
+
+issuer_address_out="${issuer_address} + 10000000"
+echo "Issuer OUTPUT: "${issuer_address_out}
+#
+# exit
+#
+echo -e "\033[0;36m Gathering UTxO Information  \033[0m"
+${cli} query utxo \
+    --testnet-magic ${testnet_magic} \
+    --address ${issuer_address} \
+    --out-file tmp/issuer_utxo.json
+
+TXNS=$(jq length tmp/issuer_utxo.json)
+if [ "$TXNS" -eq "0" ]; then
+   echo -e "\n \033[0;31m NO UTxOs Found At ${issuer_address} \033[0m \n";
+   exit;
+fi
+alltxin=""
+TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' tmp/issuer_utxo.json)
+CTXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in-collateral"' tmp/issuer_utxo.json)
+collateral_tx_in=${CTXIN::-19}
+issuer_tx_in=${TXIN::-8}
+
+echo -e "\033[0;36m Gathering Script UTxO Information  \033[0m"
+${cli} query utxo \
+    --address ${script_address} \
+    --testnet-magic ${testnet_magic} \
+    --out-file tmp/script_utxo.json
+# transaction variables
+TXNS=$(jq length tmp/script_utxo.json)
+if [ "$TXNS" -eq "0" ]; then
+   echo -e "\n \033[0;31m NO UTxOs Found At ${script_address} \033[0m \n";
+   exit;
+fi
+alltxin=""
+TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' tmp/script_utxo.json)
+script_tx_in=${TXIN::-8}
+
+script_ref_utxo=$(${cli} transaction txid --tx-file tmp/tx-reference-utxo.signed)
+
+rank_utxo=$(${cli} transaction txid --tx-file tmp/rank-tx.signed)
+# echo $rank_utxo
+# collat info
+collat_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/collat-wallet/payment.vkey)
+collat_utxo="10e5b05d90199da3f7cb581f00926f5003e22aac8a3d5a33607cd4c57d13aaf3" # in collat wallet
+
+echo -e "\033[0;36m Building Tx \033[0m"
+FEE=$(${cli} transaction build \
+    --babbage-era \
+    --protocol-params-file tmp/protocol.json \
+    --out-file tmp/tx.draft \
+    --change-address ${issuer_address} \
+    --tx-in ${issuer_tx_in} \
+    --tx-in-collateral="${collat_utxo}#0" \
+    --tx-in="${rank_utxo}#1" \
+    --spending-tx-in-reference="${script_ref_utxo}#1" \
+    --spending-plutus-script-v2 \
+    --spending-reference-tx-in-inline-datum-present \
+    --spending-reference-tx-in-redeemer-file data/redeemer/remove_redeemer.json \
+    --tx-out="${issuer_address_out}" \
+    --required-signer-hash ${issuer_pkh} \
+    --required-signer-hash ${collat_pkh} \
+    --testnet-magic ${testnet_magic})
+
+IFS=':' read -ra VALUE <<< "$FEE"
+IFS=' ' read -ra FEE <<< "${VALUE[1]}"
+FEE=${FEE[1]}
+echo -e "\033[1;32m Fee: \033[0m" $FEE
+#
+# exit
+#
+echo -e "\033[0;36m Signing \033[0m"
+${cli} transaction sign \
+    --signing-key-file wallets/seller-wallet/payment.skey \
+    --signing-key-file wallets/collat-wallet/payment.skey \
+    --tx-body-file tmp/tx.draft \
+    --out-file tmp/tx.signed \
+    --testnet-magic ${testnet_magic}
+#
+# exit
+#
+echo -e "\033[0;36m Submitting \033[0m"
+${cli} transaction submit \
+    --testnet-magic ${testnet_magic} \
+    --tx-file tmp/tx.signed
